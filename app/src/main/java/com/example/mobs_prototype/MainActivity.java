a231eefa385e6658c4bf4f0b3e81a9dbf20073c6
idlehand <trefzer.christian@gmail.com>
2024-07-31 21:48:48 +0200

    added top menu

--- app/src/main/java/com/example/mobs_prototype/MainActivity.java
+++ app/src/main/java/com/example/mobs_prototype/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.mobs_prototype;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -34,4 +38,28 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId(); // Store item ID in a variable
+
+        if (itemId == R.id.settings) {
+            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+            // Do something
+            return true;
+        } else if (itemId == R.id.logout) {
+            Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (itemId == R.id.contacts) {
+            Toast.makeText(this, "Contacts", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
